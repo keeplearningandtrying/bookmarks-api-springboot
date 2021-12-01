e6be520b562adb2c7b62606d7b455c67c044288c
sivaprasadreddy <sivaprasadreddy.k@gmail.com>
2021-12-01 17:18:33 +0530

    Rebase from heroku

--- src/main/java/com/sivalabs/bookmarks/Application.java
+++ src/main/java/com/sivalabs/bookmarks/Application.java
@@ -4,13 +4,11 @@ import com.sivalabs.bookmarks.config.ApplicationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableConfigurationProperties({ApplicationProperties.class})
 @EnableAspectJAutoProxy
-@EnableCaching
 public class Application {
 
     public static void main(String[] args) {
